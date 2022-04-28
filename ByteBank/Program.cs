677e07f8f7f8572f2d35f1530b87d39d7f444291
Batheus <batheus.dev@gmail.com>
2022-04-28 14:06:43 -0300

    Parte 4

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -12,7 +12,11 @@ namespace ByteBank
         {
             try
             {
-                ContaCorrente conta = new ContaCorrente(456, 0);
+                ContaCorrente conta = new ContaCorrente(456, 1234567);
+
+                conta.Depositar(50);
+                Console.WriteLine(conta.Saldo);
+                conta.Sacar(-500);
             }
             catch (ArgumentException ex)
             {
@@ -24,6 +28,10 @@ namespace ByteBank
                 Console.WriteLine("Argumento com problema: " + ex.ParamName);
                 Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
                 Console.WriteLine(ex.Message);
+            }
+            catch (SaldoInsuficienteException)
+            {
+                Console.WriteLine("Exceção do tipo SaldoInsuficienteException");
             }
             catch (Exception ex)
             {
